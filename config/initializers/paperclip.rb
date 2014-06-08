33502e5f2b5ba135464c82e59ae78d2a481835ae
sunnys <sunny.220990@gmail.com>
2014-06-08 20:04:59 +0530

    S3

--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -1,2 +1,4 @@
-Paperclip::Attachment.default_options[:url] = ':s3_domain_url'
+Paperclip::Attachment.default_options[:url] = 's3-ap-southeast-1.amazonaws.com'
 Paperclip::Attachment.default_options[:path] = '/:class/:attachment/:id_partition/:style/:filename'
+
+# https://s3-ap-southeast-1.amazonaws.com/spatialaccessapp/campaigns/cfiles/000/000/002/original/Adspotlisting.xlsx
