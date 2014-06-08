9c7fe18153279fe44df09eae7a699aef89fc601e
sunnys <sunny.220990@gmail.com>
2014-06-08 20:48:22 +0530

    s3

--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -1,2 +1,5 @@
-Paperclip::Attachment.default_options[:url] = 'spatialaccessapp.s3.amazonaws.com'
-Paperclip::Attachment.default_options[:path] = '/:class/:attachment/:id_partition/:style/:filename'
+# Paperclip::Attachment.default_options[:url] = ':s3_domain_url'
+# Paperclip::Attachment.default_options[:path] = '/:class/:attachment/:id_partition/:style/:filename'
+Paperclip.interpolates(:s3_sg_url) do |att, style|
+  "#{att.s3_protocol}://s3-ap-southeast-1.amazonaws.com/#{att.bucket_name}/#{att.path(style)}"
+end
